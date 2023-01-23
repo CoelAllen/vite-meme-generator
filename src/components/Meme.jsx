bbde2917b70f15348546e2f36fa57f6b9acf9d48
Coel Allen <coelallen@gmail.com>
2023-01-23 13:40:17 -0700

    text on image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -22,20 +22,34 @@ export default function Meme() {
       memeImage: url,
     }));
   }
+  function handleTopChange(event) {
+    setMeme((prev) => ({
+      ...prev,
+      topText: event?.target.value,
+    }));
+  }
+  function handleBottomChange(event) {
+    setMeme((prev) => ({
+      ...prev,
+      bottomText: event?.target.value,
+    }));
+  }
 
   return (
     <div>
-      <form className="form">
+      <form>
         <div className="meme--inputs">
           <input
             className="input--first"
             value={meme.topText}
+            onChange={handleTopChange}
             placeholder="Top text..."
             type="text"
           />
           <input
             className="input--last"
             value={meme.bottomText}
+            onChange={handleBottomChange}
             placeholder="Bottom text..."
             type="text"
           />
@@ -46,7 +60,11 @@ export default function Meme() {
           </div>
         </div>
       </form>
-      <img src={meme.memeImage} alt="Meme Image" className="meme--image" />
+      <div className="img-wrapper">
+        <img src={meme.memeImage} alt="Meme Image" className="meme--image" />
+        <p className="top-text">{meme.topText}</p>
+        <p className="bottom-text">{meme.bottomText}</p>
+      </div>
     </div>
   );
 }
